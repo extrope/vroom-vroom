ad4e78bf6d3d9d5d0edd14bf115e934436e943dd
rzrayush <ayush.chauhan252@gmail.com>
2020-11-01 20:33:26 +0000

    fin3

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+	public void ReturnMenu()
+	{
+		SceneManager.LoadScene("Menu");
+	}
+}
